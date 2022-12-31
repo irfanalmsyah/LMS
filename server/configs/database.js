5da85ad8d804f0aaf069fd9d71eaa0b722ac67dd
Irfan Alamsyah <88070126+irfanalmsyah@users.noreply.github.com>
2022-12-31 16:19:00 +0700

    add cross-env

--- server/configs/database.js
+++ server/configs/database.js
@@ -2,15 +2,33 @@ const Sequelize = require('sequelize');
 const dotenv = require('dotenv');
 dotenv.config();
 
-const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD, {
-        host: process.env.DB_HOST,
-        dialect: 'postgres',
-        logging: false
-    }
-);
+let sequelize;
+
+switch (process.env.NODE_ENV) {
+    case 'development':
+        sequelize = new Sequelize({
+            dialect: 'sqlite',
+            storage: 'database.sqlite'
+        });
+        break;
+    case 'production':
+        sequelize = new Sequelize(
+            process.env.DB_NAME,
+            process.env.DB_USER,
+            process.env.DB_PASSWORD, {
+                host: process.env.DB_HOST,
+                dialect: 'postgres',
+                logging: false
+            }
+        );
+        break;
+    default:
+        sequelize = new Sequelize({
+            dialect: 'sqlite',
+            storage: 'database.sqlite'
+        });
+        break;
+}
 
 module.exports = sequelize;
 
